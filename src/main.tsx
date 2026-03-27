ba6c855cd73fb0731d63022f884bfe52a0dddf62
Shivraj Sagade <shivrajsagade45@gmail.com>
2026-03-27 17:32:20 +0530

    fixing mobile screen

--- src/main.tsx
+++ src/main.tsx
@@ -12,15 +12,15 @@ createRoot(document.getElementById("root")!).render(
 );
 
 // ✅ Register Service Worker ONLY in production
-if ("serviceWorker" in navigator && import.meta.env.PROD) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((registration) => {
-        console.log("SW registered:", registration.scope);
-      })
-      .catch((error) => {
-        console.log("SW registration failed:", error);
-      });
-  });
-}
+//if ("serviceWorker" in navigator && import.meta.env.PROD) {
+ // //window.addEventListener("load", () => {
+    //navigator.serviceWorker
+      //.register("/sw.js")
+      //.then((registration) => {
+        //console.log("SW registered:", registration.scope);
+      //})
+      //.catch((//error) => {
+        //console.log("SW registration failed:", error);
+      //});
+  ////});
+//}
